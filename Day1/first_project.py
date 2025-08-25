ad485ce409dbd68274a94b9f8da743dd2032621c
luisemezeta <ldi.luis.munoz@gmail.com>
2025-08-25 16:45:04 -0600

    print/usr/local/bin/python3

--- Day1/first_project.py
+++ Day1/first_project.py
@@ -1,3 +1,10 @@
 print("Hello World")
 print(100+50)
 print(1000)
+
+print('Luis'+' '+'Muñoz')
+print('My name is \'Luis\'')
+print('My name is:\nLuis Muñoz')
+print('My name is \n\t Luis')
+print('This isn\'t a number')
+print('This isgn \\ is a backslash')
