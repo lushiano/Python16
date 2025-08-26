cb62824dc8db77c092f1062fd02695fed8424de1
luisemezeta <ldi.luis.munoz@gmail.com>
2025-08-26 15:45:26 -0600

    Day 1 adjustment

--- Day1/first_challenge.py
+++ Day1/first_challenge.py
@@ -1,2 +1,2 @@
 print("Let's create a name for your beer")
-print("Your beer name is '" + input("Please indicate a color: ") + " " + input("Now indicate an animal: ") + "'")
+print("Your beer name is '" + input("Please indicate a color: ") + " " + input("Now indicate an animal: ") + "'\nCongratulations!")
