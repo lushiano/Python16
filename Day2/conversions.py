a2c3f193c05d6b2feadefed694600887380a55cd
luisemezeta <ldi.luis.munoz@gmail.com>
2025-08-26 17:33:51 -0600

    String + convresions

---
+++ Day2/conversions.py
@@ -0,0 +1,20 @@
+num1 = 5.8
+print(num1)
+print(type(num1))
+
+num2 = int(num1)
+print(num2)
+print(type(num2))
+
+age = input("Tell me your age: ")
+
+print(type(age))
+
+age = int(age)
+
+print(type(age))
+
+new_age = age + age 
+print(new_age)
+
+print("Your new age is going to be " + str(new_age))
