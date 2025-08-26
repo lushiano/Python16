f87d6f81ea1631ef187c3a4d47b169ea9f8664fa
luisemezeta <ldi.luis.munoz@gmail.com>
2025-08-26 16:07:13 -0600

    Day 2

---
+++ Day2/second_day.py
@@ -0,0 +1,28 @@
+# Variables
+name = "John"
+print(name)
+
+name = "Laura"
+print(name)
+
+age=30
+print(age)
+
+age=30
+age2=15
+print(age+age2)
+
+name=input("Tell me your name: ")
+print("Your name is " + name)
+
+name ="hello "
+name2 = "Python"
+
+phrase = name + name2
+print(phrase)
+
+age = 30
+age2 = 50
+
+phrase = age + age2
+print(phrase)
