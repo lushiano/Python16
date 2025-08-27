b0739f4f4f86b90da97f794c2b832f538a540ed3
luisemezeta <ldi.luis.munoz@gmail.com>
2025-08-26 18:11:54 -0600

    Project 2 done

---
+++ Day2/second_project.py
@@ -0,0 +1,7 @@
+name = input("Whats your name? ")
+sales = input("What are your sales thies month ")
+
+sales_num = int(sales)
+com = round(sales_num*.10)
+
+print(f"Ok {name} your sales this month were {sales} so your commision is {com}, congratulations!")
