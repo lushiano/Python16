43ad9ab9fb2bcd04998da9d85e15c737ba59c161
luisemezeta <ldi.luis.munoz@gmail.com>
2025-08-26 18:07:51 -0600

    Day 2 project

---
+++ Day2/round.py
@@ -0,0 +1,17 @@
+result = round(90/7)
+print(result)
+
+rounding = round(result)
+print(rounding)
+
+print(90/7)
+print(round(90/7))
+
+value = (round(95.66666666666))
+print(type(value))
+
+value2 = 95.66666666666
+print(round(value2))
+print(type(value2))
+print(value2)
+print(type(value2))
