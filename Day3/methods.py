3f75372a71595f32826cd454ac0f690eb26a4859
luisemezeta <ldi.luis.munoz@gmail.com>
2025-08-28 16:53:07 -0600

    methods

---
+++ Day3/methods.py
@@ -0,0 +1,33 @@
+text = "We are going to learn six methods today"
+
+result = text.upper()
+print(result)
+
+result = text[4].upper()
+print(result)
+
+result = text.lower()
+print(result)
+
+result = text.split()
+print(result)
+
+result = text.split("o")
+print(result)
+
+a = "learning"
+b = "python"
+c = "is"
+d = "amazing"
+e = "-".join([a,b,c,d])
+
+print(e)
+
+result = text.find("s")
+print(result)
+
+result = text.find("q")
+print(result)
+
+result = text.replace("to","TO2")
+print(result)
