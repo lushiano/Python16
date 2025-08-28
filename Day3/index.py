83c00877e41a5a4cdec3645e8dc4406d45995dcf
luisemezeta <ldi.luis.munoz@gmail.com>
2025-08-28 16:25:32 -0600

    day 3

---
+++ Day3/index.py
@@ -0,0 +1,15 @@
+my_text = "This text is a test"
+result = my_text[-3] #position
+print(result)
+
+my_text = "This text is a test"
+result = my_text.index("text") #string or text
+print(result)
+
+my_text = "This text is a test"
+result = my_text.index("s",5,12) #from, to
+print(result)
+
+my_text = "This text is a test"
+result = my_text.rindex("s") #reverse
+print(result)
