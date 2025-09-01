4659e2bfa7901a5c41c8bffc7a66de0d9ffba396
luisemezeta <ldi.luis.munoz@gmail.com>
2025-09-01 15:39:43 -0600

    dictionaries

---
+++ Day3/dictionaries.py
@@ -0,0 +1,33 @@
+my_dictionary = {'c1':'value1','c2':'value2'}
+print(my_dictionary)
+
+result = my_dictionary['c1']
+print(result)
+
+customer = {'name':'John','last_name':'Lennon','weight':88,'height':1.80}
+
+query1 = (customer['last_name'])
+print(query1)
+
+query2 = (customer['weight'])
+print(query2)
+
+dic = {1:55,2:[10,20,30],3:{'s1':100,'s2':200}}
+print(dic[2][1])
+print(dic[3]['s2'])
+
+dic2 = {'k1':['a','b','c'],'k2':['d','e','f']}
+print(dic2['k1'][2].upper())
+
+dic3 = {1:'a',2:'b'}
+print(dic3)
+
+dic3[3] = 'c'
+print(dic3)
+
+dic3[2] = 'B'
+print(dic3)
+
+print(dic3.keys())
+print(dic3.values())
+print(dic3.items())
