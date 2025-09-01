bcb09eb157de117a3be233af156a256cd2df6a10
luisemezeta <ldi.luis.munoz@gmail.com>
2025-09-01 15:46:18 -0600

    test dictionaries

--- Day3/dictionaries.py
+++ Day3/dictionaries.py
@@ -31,3 +31,6 @@ print(dic3)
 print(dic3.keys())
 print(dic3.values())
 print(dic3.items())
+
+my_dict = {"values_1":{"v1":3,"v2":6},"points":{"points1":9,"points2":[10,300,15]}}
+print(my_dict['points']['points2'][1]) #Use dictionary indices to extract the second item of points2
