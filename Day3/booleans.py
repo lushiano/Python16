aeed2edb8c48505b7de748c027aae41755b74c0b
luisemezeta <ldi.luis.munoz@gmail.com>
2025-09-18 15:18:56 -0600

    booleans

---
+++ Day3/booleans.py
@@ -0,0 +1,31 @@
+var1 = True
+var2 = False
+
+print(type(var1))
+print(var1)
+
+number1 = 5 > 2 + 3
+print(type(number1))
+print(number1)
+
+number2 = 5 == 2 + 3
+print(type(number2))
+print(number2)
+
+number3 = 5 >= 2 + 3
+print(type(number3))
+print(number3)
+
+number4 = 5 != 2 + 3
+print(type(number4))
+print(number4)
+
+number5 = bool(5 > 6) 
+print(type(number5))
+print(number5)
+
+list = [1,2,3,4]
+control = 5 in list
+
+print(type(control))
+print(control)
