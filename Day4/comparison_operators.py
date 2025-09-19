28b24741f0705e41181316dfebec617d5a0f7813
luisemezeta <ldi.luis.munoz@gmail.com>
2025-09-19 10:15:14 -0600

    day 4

---
+++ Day4/comparison_operators.py
@@ -0,0 +1,36 @@
+# equals, not comparison, just one
+my_variable = 'hello world'
+
+# comparison, two equals
+my_bool = 10 == 25
+print(my_bool)
+
+# sum up
+my_bool = 20+5 == 25
+print(my_bool)
+
+# strings
+my_bool = "white" == "White".lower()
+print(my_bool)
+
+my_bool = 100 == '100'
+print(my_bool)
+
+my_bool = 100.0 == 100
+print(my_bool)
+
+# difference
+my_bool = 100.0 != 99
+print(my_bool)
+
+# greater
+my_bool = 100.0 > 99
+print(my_bool)
+
+# less
+my_bool = 100.0 < 99
+print(my_bool)
+
+# greater or equal
+my_bool = 5 >= 5 
+print(my_bool)
---
+++ Day4/logical_operators.py
@@ -0,0 +1,19 @@
+# and
+# or
+# not 
+
+my_bool = 4 < 5 and 5 == 2+3
+print(my_bool)
+
+my_bool = (55 == 55) and ("dog" == "dog")
+print(my_bool)
+
+my_bool = 10 == 10 or 3 == 2
+print(my_bool)
+
+text = "This sentence is short"
+my_bool = ("sentence" in text) or ("python" in text)
+print(my_bool)
+
+my_bool = not "a" == "b"
+print(my_bool)
